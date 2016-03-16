244d68eac309c220d56b7b41c0bf15e4932f888d
Pin-Hsiu <j1152776@gmail.com>
2016-03-16 15:57:49 +0800

    未完成Random走

---
+++ Assets/RandomMove.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomMove : MonoBehaviour {
+	void start(){
+		InvokeRepeating("Update",1,1);
+}
+	
+
+	void Update () {
+		float i = Random.Range (-0.2F, 0.2F);
+
+
+
+		gameObject.transform.Translate (-i, 0, i);
+	}
+
+}
